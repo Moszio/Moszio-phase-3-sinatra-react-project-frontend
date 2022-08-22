0fe8883acd6291e7baafbcad1b04a468bd48d7aa
andor <latandor@gmail.com>
2022-08-22 14:01:31 -0400

    Home Page formatting

--- src/component/Home.js
+++ src/component/Home.js
@@ -17,7 +17,26 @@ Taking logistics in a new direction with an advanced platform for both shippers 
         </div>
         <div className="home-page-section-2">
             <div className="home-page-section-2-container">
-
+                <div className="home-page-section-1-hmp-message">
+                <h1>Home!</h1>
+                <p>Moving the world, together
+Taking logistics in a new direction with an advanced platform for both shippers and carriers.</p>
+                </div>
+                <div className="home-page-section-1-hmp-image">
+                    <img src="https://images.unsplash.com/photo-1592963219751-3800a144a41e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80" alt="" />
+                </div>
+            </div>
+        </div>
+        <div className="home-page-section-3">
+            <div className="home-page-section-3-container">
+                <div className="home-page-section-1-hmp-message">
+                <h1>Home!</h1>
+                <p>Moving the world, together
+Taking logistics in a new direction with an advanced platform for both shippers and carriers.</p>
+                </div>
+                <div className="home-page-section-1-hmp-image">
+                    <img src="https://images.unsplash.com/photo-1592963219751-3800a144a41e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80" alt="" />
+                </div>
             </div>
         </div>
     </div>
