53a69a34feb0ff6f7be9e1497d4cc4514bc8d318
andor <latandor@gmail.com>
2022-08-22 17:01:56 -0400

    smmal change

--- src/component/messagebox/MessageBox.js
+++ src/component/messagebox/MessageBox.js
@@ -2,10 +2,17 @@ import MessageList from "./MessageList"
 import Header from "./Header"
 import NewMessage from "./NewMessage"
 import "./MessageBox.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 const MessageBox = () => {
+    const [messages, setMessages] = useState([])
+
+    useEffect = (() => {
+        fetch(url)
+        .then(res = res.json())
+        .then((data) = setMessages(data))
+    }, [])
 
     return (
         <div className="chatbox-container">
