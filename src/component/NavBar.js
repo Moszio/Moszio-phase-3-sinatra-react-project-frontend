92d5674afb7dddbd0bc8e175089636755eee3bda
Agnes <agsawicka@gmail.com>
2022-08-23 17:04:49 -0400

    nav bar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -12,54 +12,69 @@ const NavBar = ({logIn, setLogIn, setUserEmail, setPassword, setUserLogInName}) 
             // USER CAN LOG IN ANDOR return to home page
     }
   return (
-    
-    <div className="navbar">
-        <div className="navbar-company-name">
-            <p>FFS Logistic</p>
-        </div>
-        <div>
-            <img src="./Logo.jpg" alt="compnay-logo" className="navbar-company-logo"/>
-        </div>
-        <div className="navbar-btn-container">
-        <NavLink
-            to="/"
-            exact
-            className="navbar-btn"
-        >
-            Home
-        </NavLink>
-        <NavLink
-            to="/about"
-            exact
-            className="navbar-btn"
-        >
-            About
-        </NavLink>
 
+<nav className="navbar navbar-expand-lg navbar-light bg-light" style={{position:"fixed",width:"100%", top:"0"}}>
+
+  <div className="container-fluid">
+
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars"></i>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <a className="navbar-brand mt-2 mt-lg-0" href="#">
+        <img
+          src="./home/agnesk/Development/phase-3/Project-phase-3/Moszio-phase-3-sinatra-react-project-frontend/public/Logo.jpg"
+          height="15"
+          alt=""
+          loading="lazy"
+        />
+      </a>
+
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
+        </li>
+        
         {logIn ?  
             <>
-            <button className="navbar-btn" onClick={logOut}>Log out</button>
+            <li className="nav-item">
+                <a className="nav-link" href="/login" onClick={logOut}>Log out</a>
+            </li>
             </> 
             : 
             <>
-            <NavLink
-                to="/signUp"
-                exact
-                className="navbar-btn"
-                >
-                Sign Up
-            </NavLink>
-            <NavLink
-                to="/login"
-                exact
-                className="navbar-btn"
-                >
-                Login
-            </NavLink>
+            <li className="nav-item">
+                <a className="nav-link" href="/signup">Sign up</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/login">Log in</a>
+            </li>
             </>}
-        </div>
+      </ul>
+
     </div>
+  </div>
+
+</nav>
+  
   );
 }
 
 export default NavBar;
+
+
+
+
