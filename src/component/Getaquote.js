d1bd98764a610d7a892131835185e36765c676c4
andor <latandor@gmail.com>
2022-09-03 10:48:22 -0400

    form restriction addded

--- src/component/Getaquote.js
+++ src/component/Getaquote.js
@@ -1,14 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
 
 
 function Getaquote() {
-
+const [name, setName] = useState("")
+const [email, setEmail] = useState("")
+const [message, setMessage] = useState("")
 
 const form = useRef();
 
+const handleResetMessage = () => {
+    setMessage("")
+    setEmail("")
+    setName("")
+}
+
 const sendEmail = (e) => {
-    e.preventDefault();
+   e.preventDefault()
 
     emailjs.sendForm('service_jwq4k7s', 'template_wpe213i', form.current, 'csaTrSX49Eo0hAQao')
     .then((result) => {
@@ -16,6 +24,10 @@ const sendEmail = (e) => {
     }, (error) => {
         console.log(error.text);
     });
+
+    alert('Email sent!')
+    handleResetMessage()
+    
 };
 
 
@@ -37,20 +49,26 @@ const sendEmail = (e) => {
                             
                                 <form ref={form} onSubmit={sendEmail}>
                                     <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" id="floatingInput" placeholder="Your Name*" />
+                                    <input type="text" name='from_name' className="form-control" id="floatingInput" placeholder="Your Name*" value={name} onChange={(e) => setName(e.target.value)}/>
                                     <label htmlFor="floatingInput">Your Name*</label>
                                     </div>
                                     <div className="form-floating mb-3">
-                                    <input type="email" className="form-control" id="floatingInput" placeholder="Your Email*" />
+                                    <input type="email" name='reply_to' className="form-control" id="floatingInput" placeholder="Your Email*" value={email} onChange={(e) => setEmail(e.target.value)}/>
                                     <label htmlFor="floatingInput">Your email*</label>
                                     </div>
                                     <div className="form-floating mb-3">
-                                    <textarea className="form-control" name='message' id="floatingInput" placeholder="Message*" />
+                                    <textarea className="form-control" name='message' id="floatingInput" placeholder="Message*" 
+                                    value={message} onChange={(e) => setMessage(e.target.value)}
+                                    />
                                     <label htmlFor="floatingInput">Message*</label>
                                     </div>
                                     <div className="d-grid">
-                                    <button className="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Request a Quote</button>
+                                    {
+                                        name === "" || email ==="" || message === "" ? 
                                     
+                                    <button disabled={!message} className="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Request a Quote</button> :
+                                    <button className="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Request a Quote</button>
+}
                                     </div>
 
                                 </form>
